fbb71ea8b36e48dbf855ade4454646130b3a051e
Rxzz630 <arkarizki96@gmail.com>
2026-07-28 15:52:22 +0700

    Update ModItems.java

--- src/main/java/com/crabmod/item/ModItems.java
+++ src/main/java/com/crabmod/item/ModItems.java
@@ -1,10 +1,8 @@
 package com.crabmod.item;
 
 import com.crabmod.CrabMod;
-import com.crabmod.entity.ModEntities;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -17,17 +15,8 @@ public class ModItems {
             new Item(new Item.Settings().maxCount(16))
     );
 
-    public static final Item CRAB_SPAWN_EGG = Registry.register(
-            Registries.ITEM,
-            Identifier.of(CrabMod.MOD_ID, "crab_spawn_egg"),
-            new SpawnEggItem(ModEntities.CRAB, new Item.Settings())
-    );
-
     public static void register() {
         net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
                 .register(entries -> entries.add(CRAB_CLAW));
-
-        net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS)
-                .register(entries -> entries.add(CRAB_SPAWN_EGG));
     }
 }
