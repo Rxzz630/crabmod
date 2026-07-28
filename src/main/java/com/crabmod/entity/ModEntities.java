a5e8090c23fdaaf5589d9a745327b603d0e07179
Rxzz630 <arkarizki96@gmail.com>
2026-07-28 15:58:17 +0700

    Update ModEntities.java

--- src/main/java/com/crabmod/entity/ModEntities.java
+++ src/main/java/com/crabmod/entity/ModEntities.java
@@ -8,16 +8,21 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModEntities {
 
+    public static final RegistryKey<EntityType<?>> CRAB_KEY =
+            RegistryKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(CrabMod.MOD_ID, "crab"));
+
     public static final EntityType<CrabEntity> CRAB = Registry.register(
             Registries.ENTITY_TYPE,
-            Identifier.of(CrabMod.MOD_ID, "crab"),
+            CRAB_KEY,
             FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, CrabEntity::new)
                     .dimensions(EntityDimensions.fixed(0.6f, 0.5f))
-                    .build()
+                    .build(CRAB_KEY)
     );
 
     public static void register() {
